5a695c5f9ba0dc3ad9018c398a1db363925dbbc9
Julien Crutain <juju510@hotmail.fr>
2022-08-22 08:38:02 +0200

    add pagination and component cardPhone finish design page phoneList

---
+++ assets/components/cardPhone/CardPhone.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import { Link } from 'react-router-dom';
+import '../../styles/cardPhone.css'
+
+
+export default function CardPhone( props ) {
+    const {data} = props;
+    const [currentItems, setCurrentItems] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 6;
+
+    useEffect(() => {
+        const endOffset = itemOffset + itemsPerPage;
+        setCurrentItems(data.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(data.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage, data]);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % data.length;
+        setItemOffset(newOffset);
+    };
+
+    const newPrice = (price, percent) => {
+        const newPrice = Math.round(price - (price * (percent / 100)));
+        return newPrice
+    }
+
+    return (
+        <>
+            <div className="list__phone">
+                        {currentItems.map((phone, key) => {
+                            return (
+                                <div className="item" key={key}>
+                                    <Link className="" to={`/show/${phone.id}`}>
+                                        <div className="item__img">
+                                            <img src={`http://localhost:8000/images/phone/${phone.imageName}`} alt="image téléphone" />
+                                        </div>
+                                    </Link>
+                                    <div>
+                                        <h3>
+                                            {phone.brand} - {phone.model} - {phone.storage} Go - {phone.color}
+                                        </h3>
+                                        <p>{phone.description}</p>
+                                        <div>
+                                            <p className="item__price">
+                                                {phone.promotion > 0 ?
+                                                    <>
+                                                        <span className='item__promo--percent'>
+                                                            -{phone.promotion}%
+                                                        </span>
+
+                                                        <span className='item__price--old'>
+                                                            {phone.price} €
+                                                        </span>
+                                                        <span>
+                                                            {newPrice(phone.price, phone.promotion)} €
+                                                        </span>
+                                                    </>
+                                                    :
+                                                    <span>
+                                                        {newPrice(phone.price, phone.promotion)} €
+                                                    </span>
+                                                }
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="suivant >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                pageCount={pageCount}
+                previousLabel="< précédent"
+                renderOnZeroPageCount={null}
+                containerClassName="pagination"
+                pageLinkClassName="page-num"
+                previousLinkClassName="page-num"
+                nextLinkClassName="page-num"
+                activeLinkClassName="active"
+            />
+        </>
+    );
+}
--- assets/pages/PhoneList.js
+++ assets/pages/PhoneList.js
@@ -6,6 +6,7 @@ import AuthAPI from '../services/authApi';
 import AuthContext from '../context/AuthContext';
 import Swal from 'sweetalert2';
 import Loader from '../components/loader/loader';
+import CardPhone from '../components/cardPhone/CardPhone';
 
 export default function PhoneList() {
     const [phoneList, setPhoneList] = useState([]);
@@ -46,13 +47,8 @@ export default function PhoneList() {
             });
     }
 
-    const newPrice = (price, percent) => {
-        const newPrice = Math.round(price - (price * (percent / 100)));
-        return newPrice
-    }
-
     return (
-        <article className="container">
+        <article>
             <div>
                 <h2 className="title__h2">
                     Liste des téléphones
@@ -63,7 +59,7 @@ export default function PhoneList() {
                 :
                 <>
                     {admin &&
-                        <div className="">
+                        <div >
                             <Link className="" to="/create">
                                 Créer un nouveau téléphone
                             </Link>
@@ -76,47 +72,8 @@ export default function PhoneList() {
                             `${phoneList.length} produits`
                         }
                     </div>
-                    <div className="list__phone">
-                        {phoneList.map((phone, key) => {
-                            return (
-                                <div className="item" key={key}>
-                                    <Link className="" to={`/show/${phone.id}`}>
-                                        <div className="item__img">
-                                            <img src={`http://localhost:8000/images/phone/${phone.imageName}`} alt="image téléphone" />
-                                        </div>
-                                    </Link>
-                                    <div className='item__content'>
-                                        <h3 className="item__title">
-                                            {phone.brand} - {phone.model} - {phone.storage} Go - {phone.color}
-                                        </h3>
-                                        <p className="item__description">{phone.description}</p>
-                                        <div>
-                                            <p className="item__price">
-                                                {phone.promotion > 0 ?
-                                                    <>
-                                                        <span className='item__promo--percent'>
-                                                            -{phone.promotion}%
-                                                        </span>
 
-                                                        <span className='item__price--old'>
-                                                            {phone.price} €
-                                                        </span>
-                                                        <span className='item__price--new'>
-                                                            {newPrice(phone.price, phone.promotion)} €
-                                                        </span>
-                                                    </>
-                                                    :
-                                                    <span className='item__price--new'>
-                                                        {newPrice(phone.price, phone.promotion)} €
-                                                    </span>
-                                                }
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
+                    <CardPhone data={phoneList}/>
                 </>
             }
         </article>
